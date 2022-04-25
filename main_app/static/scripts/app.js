af4cf980f786d20ff777e6ed967a00a0ad8fe6a5
ekeith <evanmkeith@gmail.com>
2022-04-25 11:29:31 -0700

    made settings for profile page popup

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,1 +1,13 @@
-console.log("Working!");
+
+const settings_btn = document.querySelector('#settings');
+const settings_div = document.querySelector('#edit_page');
+const body = document.querySelector('body');
+
+settings.addEventListener('click', function(e){
+    e.preventDefault();
+    if(edit_page.style.display === 'none'){
+        edit_page.style.display =  'flex';
+    } else{
+        edit_page.style.display =  'none';
+    };
+})
--- tiptrip/settings.py
+++ tiptrip/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_app',
     'phonenumber_field',
+    'jquery',
 ]
 
 MIDDLEWARE = [

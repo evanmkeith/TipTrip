07f850588f14a89c366c95304aad842c579b0c83
ekeith <evanmkeith@gmail.com>
2022-04-25 15:26:13 -0700

    editing contact page style

--- main_app/views.py
+++ main_app/views.py
@@ -24,23 +24,17 @@ class About(TemplateView):
 class Profile_Form(forms.ModelForm): 
     class Meta: 
         model=Profile
-        fields = ['name', 'image_link', 'phone_number', 'zip_code', 'vehicle_type', 'vehicle_make', 'vehicle_model', 'num_seats', 'fee', 'bio']
+        fields = ['name', 'phone_number', 'zip_code']
 
 def Create_Profile(request, pk): 
     if request.method == 'POST': 
         form = Profile_Form(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
-            image_link = form.cleaned_data['image_link']
             phone_number = form.cleaned_data['phone_number']
             zip_code = form.cleaned_data['zip_code']
-            vehicle_type = form.cleaned_data['vehicle_type']
-            vehicle_make = form.cleaned_data['vehicle_make']
-            vehicle_model = form.cleaned_data['vehicle_model']
-            num_seats = form.cleaned_data['num_seats']
-            fee = form.cleaned_data['fee']
-            bio = form.cleaned_data['bio']
-            Profile.objects.create(user=User.objects.get(pk=pk), name=name, image_link=image_link, phone_number=phone_number,zip_code=zip_code, vehicle_type=vehicle_type, vehicle_make=vehicle_make, vehicle_model=vehicle_model, num_seats=num_seats, fee=fee, available=False, bio=bio)
+
+            Profile.objects.create(user=User.objects.get(pk=pk), name=name,  phone_number=phone_number,zip_code=zip_code, available=False)
             return HttpResponseRedirect('/user/'+str(pk))
         else:
             return render(request, 'create_profile.html', {'form': form})

22e41c86c28c4b52c51e685b6446610e5062f481
ekeith <evanmkeith@gmail.com>
2022-04-25 13:32:34 -0700

    Editing profile page reviews

--- main_app/views.py
+++ main_app/views.py
@@ -40,7 +40,7 @@ def Create_Profile(request, pk):
             num_seats = form.cleaned_data['num_seats']
             fee = form.cleaned_data['fee']
             bio = form.cleaned_data['bio']
-            Profile.objects.create(user=User.objects.get(pk=pk), name=name, image_link=image_link, phone_number=phone_number,zip_code=zip_code, vehicle_type=vehicle_type, vehicle_make=vehicle_make, vehicle_model=vehicle_model, num_seats=num_seats, fee=fee, bio=bio)
+            Profile.objects.create(user=User.objects.get(pk=pk), name=name, image_link=image_link, phone_number=phone_number,zip_code=zip_code, vehicle_type=vehicle_type, vehicle_make=vehicle_make, vehicle_model=vehicle_model, num_seats=num_seats, fee=fee, available=False, bio=bio)
             return HttpResponseRedirect('/user/'+str(pk))
         else:
             return render(request, 'create_profile.html', {'form': form})
